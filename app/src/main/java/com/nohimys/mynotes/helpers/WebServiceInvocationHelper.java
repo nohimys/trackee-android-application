1bebec69b0b417c94413fa586cbc6bea798de750
Nohim Sandeepa <itsnohim@gmail.com>
2017-08-23 08:01:52 +0530

    Moved WebServiceInvocation to helpers

---
+++ app/src/main/java/com/nohimys/mynotes/helpers/WebServiceInvocationHelper.java
@@ -0,0 +1,14 @@
+package com.nohimys.mynotes.helpers;
+
+/**
+ * Created by Nohim Sandeepa on 8/23/2017.
+ */
+
+public class WebServiceInvocationHelper {
+
+    private boolean isEligibleForWebCall(){
+        //TODO:
+        //Check the timing in shared preferences
+        return false;
+    }
+}
